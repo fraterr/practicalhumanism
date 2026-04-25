6110b7020955f779f3a266920c3fc240edb786ac
fraterr <practicalhumanism@gmail.com>
2026-04-26 01:01:13 +0200

    feat: customize Open Graph metadata and premium preview image

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Practical Humanism';
-export const SITE_DESCRIPTION = 'A digital grimoire for the modern technomancer.';
+export const SITE_DESCRIPTION = 'A digital laboratory dedicated to the intersection of electrons and consciousness.';
